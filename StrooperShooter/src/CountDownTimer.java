87b88c655faeb9c45ce383bd91d1296c07e619f4
Nuno Salgueiro <salgueirofil@gmail.com>
2018-06-09 15:01:50 +0100

    CountDownTimer Modified

--- StrooperShooter/src/CountDownTimer.java
+++ StrooperShooter/src/CountDownTimer.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class CountDownTimer {
+
+    public static void main(String[] args) throws Exception {
+
+
+        int i = 10;
+
+        while (i != 0) {
+            System.out.println("\b" + i);
+
+            i--;
+
+            Thread.sleep(1000);
+        }
+
+
+        System.out.println("\b*****************");
+        System.out.println("Game Over");
+        System.out.println("******************");
+
+
+    }
+
 }
