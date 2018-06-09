1e77bf6b11091a8204b9d45cfe9ae3b10eef38db
Diana Bastos <diana_s_bastos@hotmail.com>
2018-06-09 19:21:52 +0100

    mouseHandler and KeyboardHandler added

--- StrooperShooter/src/org/academiadecodigo/stormrooters/stroopershooter/Player.java
+++ StrooperShooter/src/org/academiadecodigo/stormrooters/stroopershooter/Player.java
@@ -1,5 +1,13 @@
 package org.academiadecodigo.stormrooters.stroopershooter;
 
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+import org.academiadecodigo.simplegraphics.mouse.Mouse;
+import org.academiadecodigo.simplegraphics.mouse.MouseEvent;
+import org.academiadecodigo.simplegraphics.mouse.MouseEventType;
+import org.academiadecodigo.simplegraphics.mouse.MouseHandler;
 import org.academiadecodigo.stormrooters.stroopershooter.GameObjects.GameObjects;
 
 public class Player {
@@ -8,11 +16,16 @@ public class Player {
     private int score;
     private Weapon sniper;
     private GameObjects objects;
+    private int X;
+    private int Y;
+    private boolean reload;
 
     public Player(String name) {
 
         this.name = name;
         this.sniper = new Weapon(sniper.getBulletNumber());
+        this.reload = false;
+
     }
 
     public void moveSight() {
@@ -30,11 +43,88 @@ public class Player {
 
     }
 
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
 
 
     //SETTER
     public void setScore(int score) {
 
         this.score = score;
+    }
+
+    //NESTED NESTED NESTED
+    public class WeaponControl implements MouseHandler {
+
+        private Mouse handler;
+
+        public WeaponControl() {
+
+            this.handler = new Mouse(this);
+            addEventListener();
+
+        }
+
+        public void addEventListener() {
+
+            handler.addEventListener(MouseEventType.MOUSE_CLICKED);
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+            int x = (int) e.getX();
+            int y = (int) e.getY();
+            shootWeapon();
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+        }
+    }
+
+    public class ReloadControl implements KeyboardHandler {
+
+        private Keyboard key;
+        private KeyboardEvent spaceKey;
+
+
+        public ReloadControl() {
+
+            this.key = new Keyboard(this);
+            this.spaceKey = new KeyboardEvent();
+
+            setEvent();
+            addEventListener();
+        }
+
+
+        public void setEvent() {
+
+            spaceKey.setKey(KeyboardEvent.KEY_SPACE);
+            spaceKey.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        }
+
+        public void addEventListener(){
+
+            key.addEventListener(spaceKey);
+        }
+
+        @Override
+        public void keyPressed(KeyboardEvent e) {
+
+            reload = true;
+        }
+
+
+        @Override
+        public void keyReleased(KeyboardEvent e) {
+
+        }
     }
 }
--- StrooperShooter/src/org/academiadecodigo/stormrooters/stroopershooter/Weapon.java
+++ StrooperShooter/src/org/academiadecodigo/stormrooters/stroopershooter/Weapon.java
@@ -1,11 +1,14 @@
 package org.academiadecodigo.stormrooters.stroopershooter;
 
+import org.academiadecodigo.simplegraphics.mouse.Mouse;
+import org.academiadecodigo.simplegraphics.mouse.MouseEvent;
+import org.academiadecodigo.simplegraphics.mouse.MouseEventType;
+import org.academiadecodigo.simplegraphics.mouse.MouseHandler;
 import org.academiadecodigo.stormrooters.stroopershooter.GameObjects.GameObjects;
 
 public class Weapon {
 
     private int bulletNumber;   //might be a final property?
-
 
     public Weapon(int bulletNumber) {
 
@@ -30,5 +33,4 @@ public class Weapon {
         return bulletNumber;
     }
 
-    public class WeaponControl implements
 }
