7dacf81f1352d23ad9e8d97c396ce1961f02cc56
HenryWallace4 <scp45@hotmail.com>
2018-06-09 12:38:13 +0100

    MouseTracker has the code for one timer- simpleGfx lib added

--- StrooperShooter/src/org/academiadecodigo/stormrooters/stroopershooter/Field/Field.java
+++ StrooperShooter/src/org/academiadecodigo/stormrooters/stroopershooter/Field/Field.java
@@ -1,8 +1,14 @@
 package org.academiadecodigo.stormrooters.stroopershooter.Field;
 
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 public class Field {
 
     public Field() {
+
+        Rectangle grid = new Rectangle(100, 100, 10, 10 );
+
+        grid.draw();
 
     }
 }
