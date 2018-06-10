e7f77bb254774c659194a88c6ff9655446ee6721
André Silva <andre.silva.mtc@gmail.com>
2018-06-10 17:37:42 +0100

    screen interaction work

--- StrooperShooter/src/org/academiadecodigo/stormrooters/stroopershooter/Game.java
+++ StrooperShooter/src/org/academiadecodigo/stormrooters/stroopershooter/Game.java
@@ -1,10 +1,8 @@
 package org.academiadecodigo.stormrooters.stroopershooter;
 
 import org.academiadecodigo.stormrooters.stroopershooter.Field.Grid;
-import org.academiadecodigo.stormrooters.stroopershooter.Field.Position.GridPosition;
 import org.academiadecodigo.stormrooters.stroopershooter.Field.SimplegfxGrid;
 import org.academiadecodigo.stormrooters.stroopershooter.GameObjects.Enemy;
-import org.academiadecodigo.stormrooters.stroopershooter.GameObjects.GameObjects;
 import org.academiadecodigo.stormrooters.stroopershooter.GameObjects.Target;
 
 public class Game {
@@ -12,11 +10,12 @@ public class Game {
     private int timer; //the type must be changed
     private Player player;
     private Grid grid;
-    private GameObjects gameObjects;
+    private Target objects;
     private Weapon sniper;
     //private GameObjects[] objects;
-    private Target[] objects;
+    //private Target[] objects;
     private boolean gameInit;
+    private boolean gameOn = true;
 
     //public static final int BULLET_DAMAGE = 1;
 
@@ -25,10 +24,10 @@ public class Game {
         this.sniper = new Weapon();
         this.grid = new SimplegfxGrid(124, 78);
         this.player = new Player("Batata", sniper);
-        this.objects = new Enemy[objectsNumber];
+        //this.objects = new Enemy[objectsNumber];
     }
 
-    public void init() {
+    public void init() throws InterruptedException {
 
         grid.init();
 
@@ -36,50 +35,57 @@ public class Game {
         //not working
         //they would need to be in the exact x and y to accept it as same position
         //we need to find a way to create a range
-        for (int i = 0; i < objects.length; i++) {
+/*        for (int i = 0; i < objects.length; i++) {
             objects[i] = new Enemy(grid.makeGridPosition());
 
             if (i != 0) {
                 checkPosition(i);
             }
         }
-
+*/
+        objects = new Enemy(grid.makeGridPosition());
 
         start();
     }
 
     private void checkPosition(int arrayPosition) {
 
-        for (int j = 0; arrayPosition >= j; j++) {
+/*        for (int j = 0; arrayPosition >= j; j++) {
 
             if (objects[arrayPosition].getPos().equals(objects[arrayPosition].getPos()) && arrayPosition != j) {
                 objects[arrayPosition] = new Enemy(grid.makeGridPosition());
             }
         }
-    }
+ */   }
 
-    public void start() {
+    public void start() throws InterruptedException {
 
         System.out.println("out while");
 
         
-
+/*
         player.shootWeapon(objects[0]);
         if (objects[0].isHitted()) {
             player.setScore(givePoints(0));
         }
-
-        player.shootWeapon(objects[1]);
-        if (objects[1].isHitted()) {
-            player.setScore(givePoints(1));
+*/
+        while (gameOn) {
+            Thread.sleep(100);
+        System.out.println("X:" + player.getX() + "; Y:" + player.getY());
+            if (player.getX() >= objects.getX() && player.getX() <= objects.getX() + getwidth() &&
+                    player.getY() >= objects.getY() && player.getY() <= objects.getY() + heigth()) {
+                player.shootWeapon(objects);
+                objects.hit();
+                player.setScore(objects.getPoints());
+                System.out.println("hit");
+            }
         }
-
-        System.out.println("object----" + objects[0].getX() + "..." + objects[0].getY());
+//        System.out.println("object----" + objects[0].getX() + "..." + objects[0].getY());
 
     }
 
     public int givePoints(int position) {
-        return objects[position].getPoints();
+        return objects/*[position]*/.getPoints();
     }
 
 
